f87243bcf5ead8ed3ac53581c0445e9c4a9a62fa
깃ChertHyeon <r4560798@naver.com>
2023-10-11 20:44:14 +0900

    setting

---
+++ src/main/java/com/cheor/wanted_10/Wanted10Application.java
@@ -0,0 +1,13 @@
+package com.cheor.wanted_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wanted10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wanted10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheor/wanted_10/Wanted10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheor.wanted_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wanted10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
