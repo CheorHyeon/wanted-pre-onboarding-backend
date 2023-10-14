92b9d8938833ef10614e6304adeacbd692c4a10d
cheorhyeon <r4560798@naver.com>
2023-10-14 17:03:42 +0900

    test : 지원 테스트케이스

---
+++ src/test/java/com/cheor/wanted_10/apply/controller/ApplyControllerTest.java
@@ -0,0 +1,72 @@
+package com.cheor.wanted_10.apply.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ApplyControllerTest {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	@DisplayName("채용공고 지원 테스트")
+	void t001() throws Exception {
+		ResultActions resultActions = mvc
+			.perform(
+				post("/recruitment/apply")
+					.content("""
+						{
+							"recruitmentId": 1,
+							"siteUserId": 1
+						}
+						""")
+					.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(handler().methodName("register"))
+			.andExpect(jsonPath("$.resultCode").value("S-1"))
+			.andExpect(jsonPath("$.msg").value("지원을 완료하였습니다."));
+
+		resultActions = mvc
+			.perform(
+				post("/recruitment/apply")
+					.content("""
+						{
+							"recruitmentId": 1,
+							"siteUserId": 1
+						}
+						""")
+					.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(handler().methodName("register"))
+			.andExpect(jsonPath("$.resultCode").value("F-1"))
+			.andExpect(jsonPath("$.msg").value("이미 지원한 내역이 있어 지원할 수 없습니다."));
+	}
+}
